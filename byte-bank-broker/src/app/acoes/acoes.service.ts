7a714a2feeae38fe4952653ed641aed9f82dba9c
ivanfsilva <ivanfs27@gmail.com>
2021-12-29 13:08:42 -0300

    operator map

--- byte-bank-broker/src/app/acoes/acoes.service.ts
+++ byte-bank-broker/src/app/acoes/acoes.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import {Acao} from './modelo/acoes';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +11,19 @@ export class AcoesService {
   constructor( private httpClient: HttpClient ) { }
 
   getAcoes() {
-    return this.httpClient.get<any>('http://localhost:3000/acoes');
+    return this.httpClient.get<any>('http://localhost:3000/acoes')
+      .pipe(map( (acoes) => acoes.sort( (acaoA, acaoB) => this.ordenaPorCodigo( acaoA, acaoB ) )));
+  }
+
+  private ordenaPorCodigo(acaoA, acaoB: Acao) {
+    if ( acaoA.codigo > acaoB.codigo) {
+      return 1;
+    }
+
+    if ( acaoA.codigo < acaoB.codigo) {
+      return -1;
+    }
+
+    return 0;
   }
 }
